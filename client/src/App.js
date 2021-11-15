9a6eb39a75ab1b6f911d100cca5106290b57090d
Stephen-Handy <a.king.life@pm.me>
2021-11-15 14:25:01 -0500

    app structure

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,38 @@ import './App.css';
 import React, { useState } from 'react';
 
 const App = () => {
-  
+  const [classes, setClasses] = useState();
+  console.log(classes);
 
   return(
     <div className='App'>
       <h1>Fitness App</h1>
-      <SubComp />
+      <button>Sign Up</button>
+      <button>Login</button>
+      <SubComp classes={classes} setClasses={setClasses} />
+      
     </div>
   )
 }
 
-const SubComp = () => {
+const SubComp = (props) => {
   return(
     <div className='Sub-Component'>
       <h2>Classes</h2>
+      <SubComp2 />
+    </div>
+  )
+}
+
+const SubComp2 = () => {
+  return(
+    <div className='Sub-Compoonent'>
+      <h4>Class time: </h4>
+      <h4>Date: </h4>
+      <h4>Duration: </h4>
+      <h4>Class type: </h4>
+      <h4>Intensity: </h4>
+      <h4>Location: </h4>
     </div>
   )
 }
